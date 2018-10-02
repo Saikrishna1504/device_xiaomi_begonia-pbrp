119d5842a103b6b0f85f6a916dd32fc9b8710b8e
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    begonia: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- device.mk
+++ device.mk
@@ -5,7 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/xiaomi/begonia
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
 
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
