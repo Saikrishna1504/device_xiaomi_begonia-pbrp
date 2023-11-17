758c36963516a98ed84cc273483858716abfa53a
Saikrishna1504 <saikrishna26918@gmail.com>
2023-11-17 09:26:59 +0530

    begonia: Add Build Broken Rules
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,8 @@ DEVICE_PATH := device/xiaomi/begonia
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Architecture
 TARGET_ARCH := arm64
